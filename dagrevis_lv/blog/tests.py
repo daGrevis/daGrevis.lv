39c5d7b11feae13deddcef732908f9a4af6267ed
Raitis Stengrevics <dagrevis@gmail.com>
2012-08-25 20:32:17 +0300

    Fixed URLs, fixed tests and added functionality to the view.

--- dagrevis_lv/blog/tests.py
+++ dagrevis_lv/blog/tests.py
@@ -7,21 +7,22 @@ from django.template import defaultfilters
 
 
 def create_user(username=None, password=None):
-    username = username if username != None else str(uuid4())
-    password = password if password != None else str(uuid4())
+    username = username if username else str(uuid4())
+    password = password if password else str(uuid4())
     user = User.objects.create_user(username=username, password=password)
     return user
 
 
 def create_article(user=None, title=None, content=None, slug=None):
-    user = user if user != None else create_user()
-    title = title if title != None else str(uuid4())
-    content = content if content != None else str(uuid4())
-    slug = slug if slug != None else str(uuid4())
+    user = user if user else create_user()
+    title = title if title else str(uuid4())
+    content = content if content else str(uuid4())
+    slug = slug if slug else defaultfilters.slugify(str(uuid4()))
     article = Article()
     article.user = user
     article.title = title
     article.content = content
+    article.slug = slug
     article.save()
     return article
 
@@ -49,14 +50,14 @@ class ArticleTest(TestCase):
 
         # No slug.
         response = self.client.get(reverse("blog_article", kwargs={"article_pk": article.pk}))
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(301, response.status_code)
 
         # Wrong slug.
-        response = self.client.get(reverse("blog_article", kwargs={"article_pk": article.pk}))
-        self.assertEqual(200, response.status_code)
+        response = self.client.get(reverse("blog_article", kwargs={"article_pk": article.pk, "slug": defaultfilters.slugify(str(uuid4()))}))
+        self.assertEqual(301, response.status_code)
 
         # All OK.
-        response = self.client.get(reverse("blog_article", kwargs={"article_pk": article.pk, "slug": defaultfilters.slugify(article.slug)}))
+        response = self.client.get(reverse("blog_article", kwargs={"article_pk": article.pk, "slug": article.slug}))
         self.assertEqual(200, response.status_code)
         self.assertIn(article.title, response.content)
         self.assertIn(article.content, response.content)
--- dagrevis_lv/blog/urls.py
+++ dagrevis_lv/blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns("blog.views",
     url(r"^$", "articles", name="blog_articles"),
-    url(r"^article/(?P<article_pk>\d+)/?(?P<slug>[a-z0-9-]*)/$", "article", name="blog_article"),
+    url(r"^article/(?P<article_pk>\d+)/(?P<slug>[a-z0-9-]+)/$", "article", name="blog_article"),
+    url(r"^article/(?P<article_pk>\d+)/$", "article", name="blog_article"),
 )
