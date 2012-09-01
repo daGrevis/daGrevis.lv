28ce9b1fa7ee87c2152e08b76e015cb5eadc5df6
Raitis Stengrevics <dagrevis@gmail.com>
2012-09-01 18:28:02 +0300

    Created tests and improved few of test utils.

--- dagrevis_lv/blog/tests.py
+++ dagrevis_lv/blog/tests.py
@@ -3,6 +3,7 @@ from dagrevis_lv.tests import *
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.template import defaultfilters
+from ..models import *
 
 
 class ArticleTest(TestCase):
@@ -42,4 +43,27 @@ class ArticleTest(TestCase):
 
 
 class CommentTest(TestCase):
-    pass
+    def test_no_comments(self):
+        article = create_article()
+        response = self.client.get(reverse("blog_article", kwargs={"article_pk": article.pk}))
+        expected = "No comments."
+        self.assertIn(expected, response.content)
+
+    def test_add_comment(self):
+        article = create_article()
+
+        # As guest.
+        self.client.post(reverse("blog_add_comment"), {"article_pk": article.pk, "content": str(uuid4())})
+        self.assertFalse(Article.objects.get(pk=article.pk).comment_set.exists())
+
+        # As user.
+        create_and_login_user(self.client)
+        self.client.post(reverse("blog_add_comment"), {"article_pk": article.pk, "content": str(uuid4())})
+        self.assertEqual(Article.objects.get(pk=article.pk).comment_set.count(), 1)
+
+    def test_comment_list(self):
+        create_and_login_user(self.client)
+        article = create_article()
+        for _ in range(2):
+            create_comment(article=article)
+        self.assertEqual(Article.objects.get(pk=article.pk).comment_set.count(), 2)
--- dagrevis_lv/dagrevis_lv/tests.py
+++ dagrevis_lv/dagrevis_lv/tests.py
@@ -12,6 +12,17 @@ def create_user(username=None, password=None):
     return User.objects.create_user(username=username, password=password)
 
 
+def create_and_login_user(client, username=None, password=None):
+    # Store or genarate and then store the password, because it will be needed as plain text to login.
+    password = password if password is not None else str(uuid4())
+    user = create_user(username, password)
+    client.login(username=user.username, password=password)
+
+
+def logged_in(client):
+    return "_auth_user_id" in client.session
+
+
 def create_article(user=None, title=None, content=None, slug=None):
     user = user if user is not None else create_user()
     title = title if title is not None else str(uuid4())
@@ -25,5 +36,12 @@ def create_article(user=None, title=None, content=None, slug=None):
     )
 
 
-def logged_in(client):
-    return "_auth_user_id" in client.session
+def create_comment(article=None, user=None, content=None):
+    article = article if article is not None else create_article()
+    user = user if user is not None else create_user()
+    content = content if content is not None else str(uuid4())
+    return Article.objects.create(
+        article=article,
+        user=user,
+        content=content,
+    )
