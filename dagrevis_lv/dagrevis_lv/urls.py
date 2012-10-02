82c23811da119aef225e17bd30df75986c4c3611
Raitis Stengrevics <dagrevis@gmail.com>
2012-10-02 22:33:36 +0300

    Adapted code to new standard and deleted some junk file.

--- dagrevis_lv/dagrevis_lv/urls.py
+++ dagrevis_lv/dagrevis_lv/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls import patterns, include, url
+
 
 admin.autodiscover()
 
--- dagrevis_lv/user/urls.py
+++ dagrevis_lv/user/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
+
 urlpatterns = patterns(
     "user.views",
 )
