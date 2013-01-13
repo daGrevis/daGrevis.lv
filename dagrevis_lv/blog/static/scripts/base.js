0eb5e57f313a2eb434e81be51a0186837e6062e4
Raitis Stengrevics <dagrevis@gmail.com>
2013-01-13 22:17:07 +0200

    Removed file that wasn't used.

--- dagrevis_lv/blog/static/scripts/base.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $.each($(".comment"), function(i, comment) {
-        var $comment = $(comment);
-        $comment.css("marginLeft", 20 * ($comment.data("depth")));
-    });
-});
