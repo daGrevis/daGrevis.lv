a9f8087b436d01a6e6be46a510835fe63729383c
Raitis Stengrevics <dagrevis@gmail.com>
2013-01-13 20:51:21 +0200

    Removed URL to /login/.

--- dagrevis_lv/user/urls.py
+++ dagrevis_lv/user/urls.py
@@ -4,11 +4,3 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     "user.views",
 )
-
-urlpatterns.append(
-    url(
-        r"^login/$", "django.contrib.auth.views.login",
-        {"template_name": "login.html"},
-        name="user_login",
-    ),
-)
