73ebc6767130e354e374547b977a31a8bdbe5866
Raitis Stengrevics <dagrevis@gmail.com>
2013-01-30 22:31:01 +0200

    Translation fixes that to Arons and extra validation for password field.

---
+++ dagrevis_lv/user/validators.py
@@ -0,0 +1,11 @@
+import re
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext
+
+
+def isnt_totally_dumb_password(value):
+    digit_pattern = r"\d+"
+    symbol_pattern = r"(?=[ -~])(?=[^a-zA-Z0-9])"
+    if not re.search(digit_pattern, value) or not re.search(symbol_pattern, value):
+        raise ValidationError(ugettext("This value is too weak to be used as a password."))
--- dagrevis_lv/user/views.py
+++ dagrevis_lv/user/views.py
@@ -3,17 +3,18 @@ from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.forms import UserCreationForm
+
+from user.forms import CustomUserCreationForm
 
 
 def registration(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse("user_login"))
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render_to_response(
         "registration.html",
         {"form": form},
