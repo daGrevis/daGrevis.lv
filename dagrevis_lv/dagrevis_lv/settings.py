6bc472a5976dab82ab84849896d57b1e72bfece2
Raitis Stengrevics <dagrevis@gmail.com>
2013-02-02 22:59:46 +0200

    Task #42: Legacy system

--- dagrevis_lv/dagrevis_lv/settings.py
+++ dagrevis_lv/dagrevis_lv/settings.py
@@ -15,13 +15,23 @@ MANAGERS = ADMINS
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",  # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "NAME": "database",                      # Or path to database file if using sqlite3.
+        "NAME": "default.db",                    # Or path to database file if using sqlite3.
         "USER": "",                              # Not used with sqlite3.
         "PASSWORD": "",                          # Not used with sqlite3.
         "HOST": "",                              # Set to empty string for localhost. Not used with sqlite3.
         "PORT": "",                              # Set to empty string for default. Not used with sqlite3.
-    }
+    },
+    "legacy": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "legacy",
+        "USER": "root",  # Ahh, this is so evil!
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
 }
+
+DATABASE_ROUTERS = ["core.routers.DefaultRouter", "legacy.routers.LegacyRouter"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -122,6 +132,7 @@ INSTALLED_APPS = (
     "core",
     "blog",
     "user",
+    "legacy",
 )
 
 # A sample logging configuration. The only tangible logging
