c72acaae24774bc1fd6571ac6b14cd42dd9eb0bb
Raitis Stengrevics <dagrevis@gmail.com>
2013-02-10 16:21:12 +0200

    Increased line-height.

--- dagrevis_lv/blog/templatetags/tags.py
+++ dagrevis_lv/blog/templatetags/tags.py
@@ -9,5 +9,5 @@ register = template.Library()
 @register.filter
 def get_style(tags, priority):
     max_priority = max(tags, key=lambda tag: tag["priority"])["priority"]
-    size = max_priority / 10. * priority
+    size = (max_priority / 10.) * priority
     return "font-size: {}em;".format(size)
