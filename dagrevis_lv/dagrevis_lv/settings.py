dc82b2187c62ff51451c92d3cc61b52ddfda4d6e
Raitis Stengrevics <dagrevis@gmail.com>
2013-04-20 17:35:31 +0300

    Add context processor for template (requested by Django Suit)

--- dagrevis_lv/dagrevis_lv/settings.py
+++ dagrevis_lv/dagrevis_lv/settings.py
@@ -173,6 +173,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
     "core.context_processors.settings",
 )
 
