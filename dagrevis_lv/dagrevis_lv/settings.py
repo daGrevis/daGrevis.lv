d0c1ea000ce3503ec29c99fc153c3fd5ae939ab3
Raitis Stengrevics <dagrevis@gmail.com>
2014-04-12 14:00:58 +0300

    Enable django-suit for admin

--- dagrevis_lv/dagrevis_lv/settings.py
+++ dagrevis_lv/dagrevis_lv/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sitemaps",
+    "suit",
     "django.contrib.admin",
     "social_auth",
     "gunicorn",
