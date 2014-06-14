e7e6274ee5fa16cb07e32bebe53532a6a16b7965
Raitis Stengrevics <dagrevis@gmail.com>
2014-06-14 13:11:30 +0300

    Fix tag cloud weird size

--- dagrevis_lv/blog/templatetags/tags.py
+++ dagrevis_lv/blog/templatetags/tags.py
@@ -7,5 +7,5 @@ register = template.Library()
 @register.filter
 def get_style(tags, priority):
     max_priority = max(tags, key=lambda tag: tag["priority"])["priority"]
-    size = (max_priority / 10.) * priority
+    size = 100 / max_priority / priority / 2
     return "font-size: {}em;".format(size)
