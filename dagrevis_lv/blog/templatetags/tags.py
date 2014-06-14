23ab5452078ec7a6f25dacc1459aa923887da54c
Raitis Stengrevics <dagrevis@gmail.com>
2014-06-14 18:19:23 +0300

    Fix reversed tag size

--- dagrevis_lv/blog/templatetags/tags.py
+++ dagrevis_lv/blog/templatetags/tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 
 
 register = template.Library()
@@ -7,5 +8,6 @@ register = template.Library()
 @register.filter
 def get_style(tags, priority):
     max_priority = max(tags, key=lambda tag: tag["priority"])["priority"]
-    size = 100 / max_priority / priority / 2
+    size = ((max_priority / settings.RATIO_FOR_TAG_SIZE)
+            - (100 / max_priority / priority / settings.RATIO_FOR_TAG_SIZE))
     return "font-size: {}em;".format(size)
